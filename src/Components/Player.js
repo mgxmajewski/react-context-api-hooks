d52a9bdc90cef898f6ca329f288ee01e8ae2e8a6
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-26 16:53:21 +0100

    Add hook to player {players, actions}

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ScoreboardContext} from "./Context";
 import PropTypes from 'prop-types';
 import Counter from './Counter';
 
 const Player = ({ index }) => {
 
-    return ( 
-      <div className="player">
-        <span className="player-name">
-          {/* <button 
-            className="remove-player" 
-            onClick={() => actions.removePlayer(players[index].id)}>✖
-          </button>
-          { players[index].name } */}
-        </span>
-        <Counter index={index} />
-      </div>
+    const {players, actions} = useContext(ScoreboardContext)
+
+    return (
+        <div className="player">
+            <span className="player-name">
+                <button
+                    className="remove-player"
+                    onClick={() => actions.removePlayer(players[index].id)}>✖
+                </button>
+                { players[index].name  }
+            </span>
+            <Counter index={index} />
+        </div>
     );
 };
 
 Player.propTypes = {
-  index: PropTypes.number
+    index: PropTypes.number
 };
 
 export default Player;
